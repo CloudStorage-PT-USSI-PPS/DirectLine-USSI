bbfd4ebbc45ae13948e1be299a8f5c9abc08443f
Zayan Rizki <zayanrizki14@gmail.com>
2025-09-23 06:58:54 +0000

    Try fixing this error: `Runtime Error: Error: useState is not defined. E

--- src/app/(app)/konsultasi/page.tsx
+++ src/app/(app)/konsultasi/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { useAuth } from '@/hooks/use-auth';
 import { chatHistory, users } from '@/lib/data';
